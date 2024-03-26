b0d2e00dcb136dfd9effaaae2989fe868725dda6
VladSmirnou <smr.vladislav@gmail.com>
2024-03-26 19:19:19 +0300

    remove-string-spaces(8k)

---
+++ codewars/8k/remove-string-spaces.js
@@ -0,0 +1,7 @@
+// ------------------------------ Description ------------------------------
+// Write a function that removes the spaces from the string, then
+// return the resultant string.
+// ------------------------------ Solution ------------------------------
+function noSpace(x){
+    return x.replace(/\s+/g, '');
+}
