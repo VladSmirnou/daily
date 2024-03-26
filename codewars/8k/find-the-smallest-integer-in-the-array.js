3f8ed4111825cbcf84183fad80a1c67fae927fbf
VladSmirnou <smr.vladislav@gmail.com>
2024-03-26 19:22:35 +0300

    find-the-smallest-integer-in-the-array(8k)

---
+++ codewars/8k/find-the-smallest-integer-in-the-array.js
@@ -0,0 +1,10 @@
+// ------------------------------ Description ------------------------------
+// Given an array of integers your solution should find the smallest integer.
+// You can assume, for the purpose of this kata, that the
+// supplied array will not be empty.
+// ------------------------------ Solution ------------------------------
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+      return Math.min(...args);
+    }
+}
