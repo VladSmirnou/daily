da30cc23e74ba49deb683e0a6c28f55b847a5237
VladSmirnou <smr.vladislav@gmail.com>
2024-03-27 20:16:25 +0300

    function-1-hello-world(8k)

---
+++ codewars/8k/function-1-hello-world.js
@@ -0,0 +1,4 @@
+// ------------------------------ Description ------------------------------
+// Make a simple function called greet that returns the most-famous "hello world!".
+// ------------------------------ Solution ------------------------------
+greet = () => 'hello world!';
