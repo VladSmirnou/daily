94f5c77d87ca199fcbd619012481ee121c3161d2
VladSmirnou <smr.vladislav@gmail.com>
2024-03-29 19:45:09 +0300

    beginner-lost-without-a-map(8k)

---
+++ codewars/8k/beginner-lost-without-a-map.js
@@ -0,0 +1,6 @@
+// ------------------------------ Description ------------------------------
+// Given an array of integers, return a new array with each value doubled.
+// ------------------------------ Solution ------------------------------
+function maps(x){
+    return x.map(el => el * 2);
+}
