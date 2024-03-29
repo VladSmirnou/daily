4649482a8e67a03c4cfde347926a6bc4d5512b58
VladSmirnou <smr.vladislav@gmail.com>
2024-03-29 19:51:12 +0300

    convert-a-boolean-to-a-string(8k)

---
+++ codewars/8k/convert-a-boolean-to-a-string.js
@@ -0,0 +1,8 @@
+// ------------------------------ Description ------------------------------
+// Implement a function which convert the given boolean value into its
+// string representation.
+// Note: Only valid inputs will be given.
+// ------------------------------ Solution ------------------------------
+function booleanToString(b){
+    return String(b);
+}
