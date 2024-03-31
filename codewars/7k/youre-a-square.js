d369064f7ea4c8a4f632f6575f45c3bb009c6893
VladSmirnou <smr.vladislav@gmail.com>
2024-03-31 10:54:02 +0300

    youre-a-square(7k)

---
+++ codewars/7k/youre-a-square.js
@@ -0,0 +1,14 @@
+// ------------------------------ Description ------------------------------
+// However, sometimes, you can't arrange them into a square. Instead,
+// you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
+// Task
+// Given an integral number, determine if it's a square number:
+// In mathematics, a square number or perfect square is an integer that is
+// the square of an integer; in other words, it is the product of some integer
+// with itself.
+// The tests will always use some integral number, so don't worry about that
+// in dynamic typed languages.
+// ------------------------------ Solution ------------------------------   
+var isSquare = function(n){
+    return Math.sqrt(n) % 1 === 0;
+}
