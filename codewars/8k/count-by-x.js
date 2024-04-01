ca8811ccfb34486129103bc750001ed23dba7bee
VladSmirnou <smr.vladislav@gmail.com>
2024-04-01 18:38:31 +0300

    count-by-x(8k)

---
+++ codewars/8k/count-by-x.js
@@ -0,0 +1,16 @@
+// ------------------------------ Description ------------------------------
+// Create a function with two arguments that will return an array of the first
+// n multiples of x.
+// Assume both the given number and the number of times to count will be
+// positive numbers greater than 0.
+// Return the results as an array or list ( depending on language ).
+// ------------------------------ Solution ------------------------------
+function countBy(x, n) {
+    let z = [];
+    const step = x;
+    for (;n !== 0; n--) {
+      z.push(x);
+      x += step;
+    }
+    return z;
+}
