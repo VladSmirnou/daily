2d52d348df9ad8cb5d3559a11bb67c6ecfc90c5c
VladSmirnou <smr.vladislav@gmail.com>
2024-04-01 18:28:38 +0300

    century-from-year(8k)

---
+++ codewars/8k/century-from-year.js
@@ -0,0 +1,7 @@
+// ------------------------------ Description ------------------------------
+// The first century spans from the year 1 up to and including the year 100, the
+// second century - from the year 101 up to and including the year 200, etc.
+// ------------------------------ Solution ------------------------------
+function century(year) {
+    return Math.ceil(year / 100);
+}
