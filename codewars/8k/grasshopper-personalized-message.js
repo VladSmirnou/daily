978ec4ca52cc1ec9bfc3984c03d800a084a30697
VladSmirnou <smr.vladislav@gmail.com>
2024-04-02 19:29:02 +0300

    grasshopper-personalized-message(8k)

---
+++ codewars/8k/grasshopper-personalized-message.js
@@ -0,0 +1,7 @@
+// ------------------------------ Description ------------------------------
+// Create a function that gives a personalized greeting. This function takes
+// two parameters: name and owner.
+// ------------------------------ Solution ------------------------------
+function greet (name, owner) {
+    return name === owner ? 'Hello boss' : 'Hello guest';
+}
