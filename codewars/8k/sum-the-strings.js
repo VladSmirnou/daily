fb2af5a3baa08b8e81f0b08c3a8928030b928b9a
VladSmirnou <smr.vladislav@gmail.com>
2024-04-11 18:13:46 +0300

    sum-the-strings(8k)

---
+++ codewars/8k/sum-the-strings.js
@@ -0,0 +1,8 @@
+// ------------------------------ Description ------------------------------
+// Create a function that takes 2 integers in form of a string as an input,
+// and outputs the sum (also as a string):
+// Example: (Input1, Input2 -->Output)
+// ------------------------------ Solution ------------------------------
+function sumStr(a,b) {
+    return (Number(a) + Number(b)).toString();
+}
