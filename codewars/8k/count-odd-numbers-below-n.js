3d79382689608e4aed6f4a94c663098b97705f08
VladSmirnou <smr.vladislav@gmail.com>
2024-04-13 10:17:05 +0300

    count-odd-numbers-below-n(8k)

---
+++ codewars/8k/count-odd-numbers-below-n.js
@@ -0,0 +1,7 @@
+// ------------------------------ Description ------------------------------
+// Given a number n, return the number of positive odd numbers below n, EASY!
+// Expect large Inputs!
+// ------------------------------ Solution ------------------------------
+function oddCount(n){
+    return Math.ceil((n-1)/2)
+}
