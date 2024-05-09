956d7290d9607988debceea64a0317f6e9646571
VladSmirnou <smr.vladislav@gmail.com>
2024-05-09 19:41:25 +0300

    two-to-one.js(7k)

---
+++ codewars/7k/two-to-one.js
@@ -0,0 +1,8 @@
+// ------------------------------ Description ------------------------------
+// Take 2 strings s1 and s2 including only letters from a to z. Return a new
+// sorted string, the longest possible, containing distinct letters - each
+// taken only once - coming from s1 or s2.
+// ------------------------------ Solution ------------------------------
+function longest(s1, s2) {
+    return [...new Set(s1 + s2)[Symbol.iterator]()].sort().join('');
+}
