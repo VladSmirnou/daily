640ee0f721364a0260cda0afff018f9bee254118
VladSmirnou <smr.vladislav@gmail.com>
2024-12-06 22:55:35 +0300

    Stop gninnipS My sdroW!(6k)

--- codewars/6k/tasks.js
+++ codewars/6k/tasks.js
@@ -2,3 +2,15 @@
 function solution(a, b){
     return a.length > b.length ? `${b}${a}${b}` : `${a}${b}${a}`
 }
+
+//Stop gninnipS My sdroW!
+function spinWords(string){
+    // split ' ' -> reduce [] ( lengthCheck -> (true) -> reverse -> push; (false) -> push ) -> join
+    return string.split(' ').reduce((acc, newVal) => {
+      let value = newVal;
+      if (value.length > 4) {
+        value = newVal.split('').reverse().join('');
+      }
+      return (acc.push(value), acc)
+    }, []).join(' ')
+}
