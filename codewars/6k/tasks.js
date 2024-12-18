16c53e6269fb9a15d6e3e13f14674164f15268eb
VladSmirnou <smr.vladislav@gmail.com>
2024-12-18 23:06:38 +0300

    solutions

--- codewars/6k/tasks.js
+++ codewars/6k/tasks.js
@@ -93,3 +93,83 @@ function alphabetPosition(text) {
   }
   return res.join(' ');
 }
+
+class Cuboid {
+  constructor(length, width, height) {
+    this.length = length
+    this.width = width
+    this.height = height
+  }
+  
+  get surfaceArea() {
+    return 2 * (this.width * this.length + this.width * this.height +  this.length * this.height)
+  }
+  
+  get volume() {
+    return this.length * this.width * this.height
+  }
+}
+
+class Cube extends Cuboid {
+  constructor(length) {
+    super(length, length, length)
+  }
+}
+
+Object.defineProperty(Person.prototype, 'name', {
+  get() {
+    return `${this.firstName} ${this.lastName}`;
+  },
+  set(name) {
+    ([this.firstName, this.lastName] = name.split(' '))
+  },
+})
+
+
+class Shark extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 0, "shark", status);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, status) {
+    super(name, age, 4, "cat", status);
+  }
+  
+  introduce() {
+     return `${super.introduce()}  Meow meow!`;
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, status, master) {
+    super(name, age, 4, "dog", status)
+    this.master = master
+  }
+  
+  greetMaster() {
+    return `Hello ${this.master}`
+  }
+}
+
+class Person {
+  constructor(firstName = "John", lastName = "Doe", age = 0, gender = "Male") {
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.gender = gender
+  }
+  
+  sayFullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+  
+  static greetExtraTerrestrials(raceName) {
+    return `Welcome to Planet Earth ${raceName}`
+  }
+}
+
+function factory(x){
+  return (array) => array.map(el => el * x)
+}
